526ad3fd21c43f08b9aea957c0d2005d360d5c87
JuclqSobral <juliana.sobral@ifrn.edu.br>
2024-12-26 09:37:20 -0300

    atualização

--- app/urls.py
+++ app/urls.py
@@ -1,20 +1,13 @@
 
 from django.contrib import admin
-from django.urls import path
-from genres.views import GenreCreateListView, GernreRetrieveUpdateDestroyView
-from actors.views import ActorCreateListView, ActorRetrieveUpdateDestroyView 
-from movies.views import MovieCreateListView, MovieRetrieveUpdateDestroyView
+from django.urls import path, include
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('genres/', GenreCreateListView.as_view(), name='genre-create-list'),
-    path('genres/<int:pk>/', GernreRetrieveUpdateDestroyView.as_view(), name='genre-detail-list'),
-    
-    path('actors/', ActorCreateListView.as_view(), name='actor-create-list'),
-    path('actors/<int:pk>/', ActorRetrieveUpdateDestroyView.as_view(), name='actor-detail-list'),
-    
-    path('movies/', MovieCreateListView.as_view(), name='movie-create-list'),
-    path('movies/<int:pk>/', MovieRetrieveUpdateDestroyView.as_view(), name='movie-detail-list'),
+    path('api/v1/', include('genres.urls')),
+    path('api/v1/', include('actors.urls')),
+    path('api/v1/', include('movies.urls')),
+    path('api/v1/', include('reviews.urls')),
 ]
